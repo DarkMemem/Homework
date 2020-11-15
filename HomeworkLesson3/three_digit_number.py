e2db5fbb19428147fc8670f854e0932c4e73b49d
DarkMemem <tbigbangtheorythe@gmail.com>
2020-11-15 18:34:58 +0200

    Install commit

--- HomeworkLesson3/three_digit_number.py
+++ HomeworkLesson3/three_digit_number.py
@@ -1,10 +1,18 @@
+#first option
 while True:
     number = int(input("Please enter three-digit number: "))
 
     if 1000 > number > 99:
         tmp_number = str(number)
         tmp_number = int(tmp_number[::-1])
-        print(int(tmp_number))
+        print("Yor number:", int(tmp_number), sep="")
         break
     else:
         print("Repeat please")
+
+#second option
+dir1 = number % 10
+dir2 = number // 10 % 10
+dir3 = number // 100
+print("Yor number:", dir1, dir2, dir3, sep="")
+
