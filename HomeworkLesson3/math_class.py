0372d75e3562a2f498f64a7c0711255c55b3966d
DarkMemem <tbigbangtheorythe@gmail.com>
2020-11-15 22:32:37 +0200

    Install commit

--- HomeworkLesson3/math_class.py
+++ HomeworkLesson3/math_class.py
@@ -2,20 +2,20 @@ class1 = int(input("Enter the number of students in the first class: "))
 class2 = int(input("Enter the number of students in the second class: "))
 class3 = int(input("Enter the number of students in the third class: "))
 
-Desk_in_first_class = class1 // 2
-Desk_in_second_class = class2 // 2
-Desk_in_third_class = class3 // 2
+Desk_in_first_class = class1 / 2
+Desk_in_second_class = class2 / 2
+Desk_in_third_class = class3 / 2
 
-if Desk_in_first_class % 1 < 0.1:
+if Desk_in_first_class % 1 == 0.5:
     Desk_in_first_class += 1
 
-if Desk_in_second_class % 1 < 0.1:
+if Desk_in_second_class % 1 == 0.5:
     Desk_in_second_class += 1
 
-if Desk_in_third_class % 1 < 0.1:
+if Desk_in_third_class % 1 == 0.5:
     Desk_in_third_class += 1
 
-print("First class requires " + str(Desk_in_first_class) + " desk")
-print("Second class requires " + str(Desk_in_second_class) + " desk")
-print("Third class requires " + str(Desk_in_third_class) + " desk")
+print("First class requires ", int(Desk_in_first_class), " desk")
+print("Second class requires ", int(Desk_in_second_class), " desk")
+print("Third class requires ", int(Desk_in_third_class), " desk")
 
